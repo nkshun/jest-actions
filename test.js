64e045d65029cf9d37ae39b29d1730d0a440ecdb
中村駿介 <nakamurashunsuke@shunsuke-MacBook-Pro.local>
2021-09-21 14:26:45 +0900

    k6テスト追加

--- test.js
+++ test.js
@@ -6,6 +6,9 @@ export default function() {
     "http://localhost:3000/"
   ]);
 
+  console.log(responses[0].status);
+  console.log(responses[0].body);
+
   check(responses[0], {
     "ok status": res => res.status === 400,
     "ok text": res => res.body === 'Who are you?',
